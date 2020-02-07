2606fce744017ba49c7615d86c027634a91a8b5a
ranadheer thokala <36018702+randheerreddyt@users.noreply.github.com>
2020-02-06 18:36:20 -0800

    Update BinarySearchTree.java

--- lamaspringboot/src/main/java/com/springboot/lamaspringboot/BinarySearchTree.java
+++ lamaspringboot/src/main/java/com/springboot/lamaspringboot/BinarySearchTree.java
@@ -206,6 +206,37 @@ public class BinarySearchTree {
 
         return bigger + 1;
     }
+    
+    int sum(Node node)  
+    { 
+        if (node == null) 
+            return 0; 
+        return sum(node.left) + node.data + sum(node.right); 
+    } 
+   
+    /* returns 1 if sum property holds for the given 
+       node and both of its children */
+    int isSumTree(Node node)  
+    { 
+        int ls, rs; 
+   
+        /* If node is NULL or it's a leaf node then 
+           return true */
+        if ((node == null) || (node.left == null && node.right == null)) 
+            return 1; 
+   
+        /* Get sum of nodes in left and right subtrees */
+        ls = sum(node.left); 
+        rs = sum(node.right); 
+   
+        /* if the node and both of its children satisfy the 
+           property return 1 else 0*/
+        if ((node.data == ls + rs) && (isSumTree(node.left) != 0) 
+                && (isSumTree(node.right)) != 0) 
+            return 1; 
+   
+        return 0; 
+    } 
 
 //    public BSTNode swapTwoNodes(BSTNode firstNode, BSTNode secNode){
 //            BSTNode temp = secNode;
