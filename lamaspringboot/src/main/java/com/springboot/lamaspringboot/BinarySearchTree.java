d1092483bc8435a250678be542dfa34e6ea4d2d3
ranadheer thokala <36018702+randheerreddyt@users.noreply.github.com>
2020-02-06 18:59:52 -0800

    Update BinarySearchTree.java

--- lamaspringboot/src/main/java/com/springboot/lamaspringboot/BinarySearchTree.java
+++ lamaspringboot/src/main/java/com/springboot/lamaspringboot/BinarySearchTree.java
@@ -266,6 +266,29 @@ public class BinarySearchTree {
 
         return tree;
     }
+    
+    public boolean findTarget(TreeNode root, int k) {
+        List < Integer > list = new ArrayList();
+        inorder(root, list);
+        int l = 0, r = list.size() - 1;
+        while (l < r) {
+            int sum = list.get(l) + list.get(r);
+            if (sum == k)
+                return true;
+            if (sum < k)
+                l++;
+            else
+                r--;
+        }
+        return false;
+    }
+    public void inorder(TreeNode root, List < Integer > list) {
+        if (root == null)
+            return;
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
 
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree();
